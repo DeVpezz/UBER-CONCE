181e1edf4df8a90567637749689589c69ab3392b
Hari Prakash Singh <programmdevv@gmail.com>
2025-03-15 17:18:02 +0530

    registration,login,profile,logout

--- Backend/app.js
+++ Backend/app.js
@@ -4,10 +4,23 @@ const express=require(`express`)
 const cors=require('cors')
 const app=express()
 
+const connectToDb=require('./db/db')
+const userRoutes = require('./routes/user.routes');
+const cookieParser = require('cookie-parser')
+
+
+connectToDb()
+
 app.use(cors())
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get(`/`,(req,res)=>{
     res.send("hello world this is our setup")
 })
+
+
+app.use('/users', userRoutes);
 
 module.exports=app;
