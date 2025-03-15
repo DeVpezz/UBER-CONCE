2302cfbc1bdcf4ff7be8384b1ec10e718bf31d8f
Hari Prakash Singh <programmdevv@gmail.com>
2025-03-15 20:38:28 +0530

    sign and login in pages

--- Backend/app.js
+++ Backend/app.js
@@ -3,10 +3,11 @@ dotenv.config()
 const express=require(`express`)
 const cors=require('cors')
 const app=express()
-
-const connectToDb=require('./db/db')
-const userRoutes = require('./routes/user.routes');
 const cookieParser = require('cookie-parser')
+const connectToDb=require('./db/db')
+const userRoutes = require('./routes/user.routes')
+const captainRoutes = require('./routes/captain.routes');
+
 
 
 connectToDb()
@@ -22,5 +23,6 @@ app.get(`/`,(req,res)=>{
 
 
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 module.exports=app;
